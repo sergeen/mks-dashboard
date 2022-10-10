20277c91cb48fec78731bf3a19644338a87df590
Sergio Scotta <sergeen@gmail.com>
2022-10-10 20:44:47 -0300

    Styled most of the chart component, added a temp color helper until I get styled-componets set-up

--- src/components/HighCharts.tsx
+++ src/components/HighCharts.tsx
@@ -1,16 +1,77 @@
 import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 
+import Colors from "../helpers/colors";
+
 const options = {
   chart: {
     type: "spline",
   },
+  legend: {
+    enabled: false,
+  },
   title: {
     text: "My chart",
   },
+  tooltip: {
+    backgroundColor: Colors.primary.white,
+    borderWidth: 1,
+    borderColor: Colors.lightTheme.lightGrayishBlueCardBg,
+    borderRadius: 0,
+    shadow: false,
+    shape: "rect",
+    formatter: function () {
+      return `<p style="color:${Colors.lightTheme.desaturatedPurpleChart}">${this.y} new followers</p>`;
+    },
+    positioner: function (a, b, point) {
+      if (point.plotX - 70 > this.chart.plotLeft) {
+        return {x: point.plotX, y: point.plotY + b * 2};
+      } else {
+        return {x: point.plotX + 100, y: point.plotY + 25};
+      }
+    },
+  },
+  plotOptions: {
+    xAxis: {
+      max: 13,
+      min: 3.5,
+    },
+    yAxis: {
+      title: undefined, // Why it's not working?
+      max: 12,
+      min: 0,
+    },
+    series: {
+      pointStart: 4,
+      allowPointSelect: true,
+      lineWidth: 1,
+      color: Colors.lightTheme.desaturatedPurpleChart,
+      states: {
+        hover: {
+          enabled: false,
+        },
+      },
+      marker: {
+        fillColor: Colors.primary.white,
+        lineWidth: 2,
+        lineColor: null,
+        radius: 5,
+        states: {
+          select: {
+            fillColor: Colors.lightTheme.desaturatedPurpleChart,
+            lineWidth: 0,
+            radius: 7.5,
+          },
+          hover: {
+            enabled: false,
+          },
+        },
+      },
+    },
+  },
   series: [
     {
-      data: [1, 2, 1, 4, 3, 6],
+      data: [[3, 4], 3, 2, 6, 8, 9, 10, 9, 10, 12, 12],
     },
   ],
 };
---
+++ src/helpers/colors.tsx
@@ -0,0 +1,35 @@
+const Colors = {
+  primary: {
+    white: "hsl(0, 0%, 100%)",
+    limeGreen: "hsl(163, 72%, 41%)",
+    brightRed: "hsl(356, 69%, 56%)",
+    facebook: "hsl(195, 100%, 50%)",
+    twitter: "hsl(203, 89%, 53%)",
+    instagram: "linear-gradient(hsl(37, 97%, 70%),hsl(329, 70%, 58%))",
+    youTube: "hsl(348, 97%, 39%)",
+  },
+  neutral: {
+    darkGrey: "hsl(228, 2%, 40%)",
+    dashedGrey: "hsl(330, 3%, 85%)",
+    intermedianGrey: "hsl(224, 7%, 55%)",
+    lightGrey: "hsl(230, 75%, 98%)",
+    mediumGrey: "hsl(222, 50%, 96%)",
+  },
+  darkTheme: {
+    toggle: "linear-gradient(hsl(210, 78%, 56%),hsl(146, 68%, 55%))",
+    veryDarkBlueBg: "hsl(230, 17%, 14%)",
+    veryDarkBlueTopBgPattern: "hsl(232, 19%, 15%)",
+    darkDesaturatedBlueCardBg: "hsl(228, 28%, 20%)",
+    desaturatedBlueText: "hsl(228, 34%, 66%)",
+    desaturatedPurpleChart: "hsl(243, 51%, 70%)",
+  },
+  lightTheme: {
+    veryPaleBlueTopBgPattern: "hsl(225, 100%, 98%)",
+    lightGrayishBlueCardBg: "hsl(227, 47%, 96%)",
+    darkGrayishBlueText: "hsl(228, 12%, 44%)",
+    veryDarkBlueText: "hsl(230, 17%, 14%)",
+    desaturatedPurpleChart: "hsl(243, 51%, 70%)",
+  },
+};
+
+export default Colors;
